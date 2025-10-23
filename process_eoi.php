8bcf062436810f8fffa930c3f45ec9da0863fdd2
Jonah Clyde <105671294@student.swin.edu.au>
2025-10-23 13:13:56 +1100

    comment process_eoi

--- process_eoi.php
+++ process_eoi.php
@@ -6,13 +6,13 @@
             require_once('settings.php');
 
 
-            function leaveprocess(){
+            function leaveProcess(){
                 header("Location: apply.php");
                 exit();
             }
 
             if (!isset($_POST['referenceNumber'])) {
-                leaveprocess();
+                leaveProcess();
             } 
             function validate($value, $type, $arg, $regex) {
                 // validate("Jonah", "VARCHAR", 40, "[a-zA-Z]{1,20}")
@@ -20,61 +20,59 @@
                 $value = trim($value);
                 $value = stripslashes($value);
                 $value = htmlspecialchars($value);
-                // TYPE (int, varchar, phonenum)
 
                 if ($type == "VARCHAR" || $type == "INT" && $arg != 0){
                     $length = strlen($value);
 
                     if ($length > $arg) { 
-                        //echo $value . "Is larger than needed";
-                        //return -1; 
-                        leaveprocess(); 
+                        //Is larger than needed
+                        leaveProcess(); 
                     }
                 } else if ($type == "PHONENUM") {
                     $length = strlen($value);
                     if ($length > 12 && $length < 8) { 
-                        //echo $value . "Is too long";
-                        //return -1; 
-                        leaveprocess(); 
+                        //Is too long
+                        leaveProcess(); 
                     }
-                } // else { $correctValues = true; } // TEXT
+                } 
 
                 // REGEX
                 if (preg_match($regex, $value) == false && $regex != "") { 
-                    //echo "<p>" . $value . "does not have regex for" . $regex . "</p>";
-                    //return -1; 
-                    leaveprocess(); 
+                    // value does not have regex for specified regex
+                    leaveProcess(); 
                 }
 
                 // NULL CHECK
                 if ($value == "" && $type != "TEXT") { 
-                    //echo "Null Value";
-                    //return -1; 
-                    leaveprocess();
+                    // Null Value
+                    leaveProcess();
                 }
                 
                 return $value;
             }
 
             function sqlquery($host, $user, $pwd, $sql_db, $query, $return) {
-
+                // Establish connection with database
                 $conn = mysqli_connect($host, $user, $pwd, $sql_db);
 
                 if (!$conn){
                     echo "<p> Could not connect to the database. Please try again</p>";
                 }
+                // Query database
                 $result = mysqli_query($conn, $query);
 
+                // Close connection and return
                 mysqli_close($conn);
                 if ($return){
                     return $result;
                 }
                 
             }
-
+            // Does table exist?
             $query = "CREATE TABLE IF NOT EXISTS `eoi` (`eoi_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, `job_reference_number` varchar(5) NOT NULL, `first_name` varchar(20) NOT NULL, `last_name` varchar(20) NOT NULL, `dob` date NOT NULL, `gender` varchar(6) NOT NULL COMMENT 'MALE / FEMALE', `address` varchar(40) NOT NULL, `suburb` varchar(40) NOT NULL, `state` varchar(3) NOT NULL, `postcode` int(4) NOT NULL, `email` text NOT NULL, `phone_number` varchar(12) NOT NULL COMMENT 'Min 8, Max 12', `skill_list` text NOT NULL COMMENT 'checkbox inputs', `other_skills` text NOT NULL, `status` varchar(7) NOT NULL COMMENT 'new / current / final') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
             sqlquery($host, $user, $pwd, $sql_db, $query, false);
 
+            // Import Values from Submission
             $job_ref_num = $_POST['referenceNumber'];
             $first_name = $_POST['firstName'];
             $last_name = $_POST['lastName'];
@@ -87,10 +85,13 @@
             $phone_num = $_POST['number'];
             $skills = $_POST['skills'];
 
+            // encode skills for import into database
             $jsonData = json_encode($skills);
 
             $other_skill = $_POST['otherskills'];
 
+
+            // Validate fields for required fields and database entry
             $job_ref_num = validate($job_ref_num, "VARCHAR", 5, "/[A-Za-z0-9]{5}/");
             $first_name = validate($first_name, "VARCHAR", 20, "/[A-Za-z]{1,20}/");
             $last_name = validate($last_name, "VARCHAR", 20, "/[A-Za-z]{1,20}/");
@@ -105,7 +106,7 @@
             sqlquery($host, $user, $pwd, $sql_db, $query, false);
 
 
-            // Find ID number
+            // Find ID number generated
             $query = "SELECT eoi_id FROM `eoi` WHERE first_name = '" . $first_name . "' AND last_name = '" . $last_name . "' ORDER BY eoi_id DESC LIMIT 1;"; // Order created by ChatGPT
             $conn = mysqli_connect($host, $user, $pwd, $sql_db);
             $result = mysqli_query($conn, $query);
@@ -116,9 +117,7 @@
                 $row = mysqli_fetch_assoc($result);
                 $latest_id = $row['eoi_id'];
                 echo "<p>Your Application Number Is: " . $latest_id . "</p>";
-            } else {
-                //echo "No rows found";
-            }            
+            }        
         ?>
     </body>
 </html>
